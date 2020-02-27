7c5d54ac45f7595131a81e132a8503661f945f52
FstSlacker <61512571+FstSlacker@users.noreply.github.com>
2020-02-27 10:58:11 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 26 20:37:53 2020
-
-@author: Konstantin
-"""
 import telebot
-token='1137283074:AAG-j7YVn-qDj5GYaFKzyFkWGesrVSFVT7E'
+token=''
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start', 'go'])
